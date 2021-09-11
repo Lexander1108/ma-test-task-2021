4a5b5eb95481407e720c64fa12adec4813f9d048
Alexander Kretov <alexander.retov99@gmail.com>
2021-09-11 16:13:32 +0300

    style: eslint fixes

--- index.js
+++ index.js
@@ -1,75 +1,63 @@
-const goods = require('./goods.json');
+const arrayOfGoods = require('./goods.json');
 
-function handleGoods (goods) {
-    const filteredArray = goods.filter(function(good){
-        const isItemString = typeof good.item === 'string';
-        return isItemString;
+function handleGoods(goods) {
+  const filteredArray = goods.filter((good) => {
+    const isItemString = typeof good.item === 'string';
+    return isItemString;
+  });
+
+  const sortedGoods = [...filteredArray].sort((first, second) => (first.item > second.item ? 1 : -1));
+
+  const totalWatermelons = filteredArray
+    .filter((good) => {
+      const watermelon = good.item === 'watermelon';
+      return watermelon;
     })
+    .map((good) => good.quantity)
+    .reduce((acc, val) => acc + val, 0);
 
-    const sortedGoods = [...filteredArray].sort(function(first, second){
-        return first.item > second.item ? 1 : -1;
+  console.log('Watermelons -', totalWatermelons);
+
+  const totalApplesWeight = filteredArray
+    .filter((good) => {
+      const apple = good.item === 'apple';
+      return apple;
     })
+    .map((good) => good.weight)
+    .reduce((acc, val) => acc + val);
 
-    const totalWatermelons = filteredArray.filter(function(good){
-        const watermelon = good.item === 'watermelon';
-        return watermelon;
-    })
-        .map(function(good){
-            return good.quantity;
-        })
-        .reduce(function(acc, val){
-            return acc + val;
-        },0);
-    
-    console.log('Watermelons -',  totalWatermelons);
+  console.log('Apples - ', totalApplesWeight);
 
-    const totalApplesWeight = filteredArray.filter(function(good){
-        const apple = good.item === 'apple';
-        return apple;
-    })
-        .map(function(good){
-            return good.weight;
-        })
-        .reduce(function(acc, val){
-            return acc + val;
-        })
-    
-    console.log('Apples - ', totalApplesWeight);
+  function costOfGood(good) {
+    return (good.pricePerKilo || good.pricePerItem).replace(',', '.').replace('$', '') * (good.weight || good.quantity);
+  }
 
-    function costOfGood (good){
-        return (good.pricePerKilo || good.pricePerItem).replace(',', '.').replace('$', '') * (good.weight || good.quantity);
-        
-    }
+  filteredArray.forEach((good) => {
+    console.log(good.item, 'price is', costOfGood(good), '$');
+  });
 
-    const price = filteredArray.forEach(function(good){
-        console.log(good.item, 'price is', costOfGood(good), '$') 
-    })
-
-    const cheapestOrange = filteredArray.filter(function(good){
-        if (good.item === 'orange') {
-            return true;
-        }
-    }).sort(function(item1, item2){
-        const costOfFirst = costOfGood(item1);
-        const costOfSecond = costOfGood(item2);
-        return costOfFirst - costOfSecond;
+  const cheapestOrange = filteredArray
+    .filter((good) => good.item === 'orange')
+    .sort((item1, item2) => {
+      const costOfFirst = costOfGood(item1);
+      const costOfSecond = costOfGood(item2);
+      return costOfFirst - costOfSecond;
     })[0].type;
 
-    console.log(cheapestOrange, 'is the cheapest orange');
-    
-    const costOfGoods = [...filteredArray].sort(function(item1, item2){
-        const costOfFirst = costOfGood(item1);
-        const costOfSecond = costOfGood(item2);
-        return costOfFirst - costOfSecond;
-            
-    })
+  console.log(cheapestOrange, 'is the cheapest orange');
 
-    return {
-        filteredArray,
-        sortedGoods,
-        costOfGoods,
-    }
+  const costOfGoods = [...filteredArray].sort((item1, item2) => {
+    const costOfFirst = costOfGood(item1);
+    const costOfSecond = costOfGood(item2);
+    return costOfFirst - costOfSecond;
+  });
+
+  return {
+    filteredArray,
+    sortedGoods,
+    costOfGoods,
+  };
 }
 
-const result = handleGoods(goods);
+const result = handleGoods(arrayOfGoods);
 console.log(result);
